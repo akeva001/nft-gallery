987e1dc1ed5a841229084827e1f3fed498e75505
Alex Kevakian <akeva001@ucr.edu>
2023-04-29 21:23:06 -0700

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
